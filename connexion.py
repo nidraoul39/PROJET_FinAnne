a5a7a08982aab422a1513e9c610166aa86250e61
nidraoul39 <117579291+nidraoul39@users.noreply.github.com>
2024-04-29 19:43:11 +0200

    Update connexion.py

--- connexion.py
+++ connexion.py
@@ -2,31 +2,9 @@ import socket
 import pickle
 
 class Client:
-    def __init__(self):
+    def __init__(self,ip:str,port:int):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = ("82.67.59.220", 5555)
-        self.socket.settimeout(3600)
-
-    def envoie_donnees(self, donnees):
-        self.socket.send(pickle.dumps(donnees))
-    
-    def recevoir_donnees(self):
-        donnees = self.socket.recv(1024)
-        return pickle.loads(donnees)
-    
-    def connexion(self):
-        self.socket.connect(self.host)
-
-    def fermer_connexion(self):
-        self.socket.close()
-
-import socket
-import pickle
-
-class Client:
-    def __init__(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = ("82.67.59.220", 5555)
+        self.host = (ip, port)
         self.socket.settimeout(3600)
 
     def envoie_donnees(self, donnees):
